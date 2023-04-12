8145b469cd294e8e3dc68373cffac78981277d70
iWolfer <iwolfer@me.com>
2023-04-12 19:55:18 +0300

    Частина кода для сервера

---
+++ Lesson11/task1_client.py
@@ -0,0 +1,15 @@
+import socket
+
+# Створення клієнта та підключення до сервера
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('localhost', 5555))
+
+# Відправлення повідомлень від клієнта до сервера та приймання повідомлень від сервера
+while True:
+    message = input()
+    client_socket.sendall(message.encode('utf-8'))
+    data = client_socket.recv(1024)
+    print(data.decode('utf-8'))
+
+# Закриття з'єднання з клієнтом та сервером
+client_socket.close()
---
+++ Lesson11/task1_server.py
@@ -0,0 +1,19 @@
+import socket
+
+# Створення сервера
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('localhost', 5555))
+server_socket.listen(1)
+
+# Очікування підключення клієнта та приймання повідомлень від нього
+while True:
+    client_socket, client_address = server_socket.accept()
+    print('Connected by', client_address)
+    while True:
+        data = client_socket.recv(1024)
+        if not data: break
+        print(data.decode('utf-8'))
+
+# Закриття з'єднання з клієнтом та сервером
+client_socket.close()
+server_socket.close()
