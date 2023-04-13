d82cc5c5d24bf0fe2087bdff3b4d7a70bab8ce85
iWolfer <iwolfer@me.com>
2023-04-13 15:34:59 +0300

    Добавили до коду сервер з першого завдання, код з питаннями та відповідями

--- Lesson11/server1.py
+++ Lesson11/server1.py
@@ -7,8 +7,16 @@ messages = []
 @app.route('/send_message', methods=['POST'])
 def send_message():
     message = request.get_json().get('message')
-    messages.append(message)
-    return jsonify({'status': 'OK'})
+    if message.lower() == 'привіт':
+        response = 'Привіт! Як справи?'
+    elif message.lower() == 'як тебе звати?':
+        response = 'Мене звати Чат-бот. А як тебе?'
+    elif 'погода' in message.lower():
+        response = 'Зараз сонячно і тепло!'
+    else:
+        response = 'Дякую за повідомлення!'
+    messages.append({'user': message, 'bot': response})
+    return jsonify({'status': 'OK', 'response': response})
 
 @app.route('/get_messages', methods=['GET'])
 def get_messages():
