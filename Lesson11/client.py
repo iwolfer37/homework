77ac2e989e59e68a11ad0c7490a316ed03bf5f6e
iWolfer <iwolfer@me.com>
2023-04-13 17:29:13 +0300

    виправив код

--- Lesson11/client.py
+++ Lesson11/client.py
@@ -5,7 +5,7 @@ while True:
     message = input('Введіть повідомлення: ')
     requests.post('http://localhost:5000/send_message', json={'message': message})
     time.sleep(1)
-    response = requests.get('http://localhost:5000/get_messages')
+    response = requests.get('http://localhost:5555/get_messages')
     messages = response.json().get('messages')
     for message in messages:
         print(message)
