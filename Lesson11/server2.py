a6aa821c3689c81932e11ac1e65ecf46632bbdb3
iWolfer <iwolfer@me.com>
2023-04-13 16:06:15 +0300

    правки в код

---
+++ Lesson11/server2.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonfly
+
+app = Flask(__name__)
+
+@app.route('/wordcount', methods=['POST'])
+def word_count():
+    data = request.get_json()
+    phrase = data['phrase']
+    word_count = len(phrase.split())
+    return {'word_count': word_count}
+
+if __name__ == '__main__':
+    app.run()
