2aea3857fe4d74ebd125a77718f8bbfdd1511566
iWolfer <iwolfer@me.com>
2023-04-21 19:29:11 +0300

    дз 15 створюю попередні файли

---
+++ Lesson15/client1.py
@@ -0,0 +1,12 @@
+#Клієнт для завдання 1
+import requests
+import time
+
+while True:
+    message = input('Введіть повідомлення: ')
+    requests.post('http://localhost:5000/send_message', json={'message': message})
+    time.sleep(1)
+    response = requests.get('http://localhost:5000/get_messages')
+    messages = response.json().get('messages')
+    for message in messages:
+        print(message)
---
+++ Lesson15/task1.py
@@ -0,0 +1,33 @@
+import logging
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+messages = []
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.get_json().get('message')
+    if message.lower() == 'Слава нації':
+        response = 'Смерть ворогам'
+    elif 'нації' in message.lower():
+        response = 'Смерть ворогам'
+    else:
+        response = 'Героям Слава!!'
+    messages.append({'user': message, 'bot': response})
+    logger.info('User sent message: %s', message)
+    logger.info('Bot responded with: %s', response)
+    return jsonify({'status': 'OK', 'response': response})
+
+@app.route('/get_messages', methods=['GET'])
+def get_messages():
+    return jsonify({'messages': messages})
+
+if __name__ == '__main__':
+    app.run(debug=True)
