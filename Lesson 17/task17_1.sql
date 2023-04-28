869a02da5319625fdd301cf58b14d48f84cc46f4
iWolfer <iwolfer@me.com>
2023-04-28 14:22:00 +0300

    через datagrip database ствр

---
+++ Lesson 17/task17_1.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*) AS vacancy_count
+FROM employee
+WHERE hire_date IS NULL;
---
+++ Lesson 17/task17_2.sql
@@ -0,0 +1,3 @@
+SELECT AVG(salary) AS avg_salary, COUNT(*) AS employee_count
+FROM employee
+WHERE department_id = 90;
